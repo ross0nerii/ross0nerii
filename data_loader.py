e9259c119b58d639af0a5d6ec129d3810ebe4562
ross0nerii <acrossonerii@gmail.com>
2025-12-06 16:28:53 +0300

    Add error handling

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,5 @@
 # Code updated at 2025-11-27 21:40:25 | ID: 3219
 # Code updated at 2025-11-27 21:40:33 | ID: 2673
 # Code updated at 2025-11-27 21:40:34 | ID: 5433
+
+# Last edit: 2025-12-06 16:28:53 | Ref: 537
